0a3ab822ba685bd55797837bed0157dbf0e42ff5
kdh82 <hwanta112@naver.com>
2017-03-07 10:50:25 +0900

    tag ver02 테스트

--- src/hello_study/Hello_test.java
+++ src/hello_study/Hello_test.java
@@ -7,5 +7,9 @@ public class Hello_test {
 		System.out.println("이상한 나라에 지구인들");
 		System.out.println("Git Hub Edit"); 
 		System.out.println("last local repository");
-				   }
+		
+		
+		//
+		System.out.println("Ver02 테스트 ");
+	}
 }
