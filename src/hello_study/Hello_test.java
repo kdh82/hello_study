763cb6b210dfdc911e8e5501e6825b33b8d3591a
kdh82 <hwanta112@naver.com>
2017-03-07 10:31:47 +0900

    Update Hello_test.java
    깃허브에서 수정

--- src/hello_study/Hello_test.java
+++ src/hello_study/Hello_test.java
@@ -4,6 +4,6 @@ public class Hello_test {
 	public static void main(String[] args) {
 		System.out.println("이상한 나라에 김두환");
 		System.out.println("hello to test");
-		System.out.println("git Hub edit");
+		System.out.println("git Hub edit"); 
 	}
 }
